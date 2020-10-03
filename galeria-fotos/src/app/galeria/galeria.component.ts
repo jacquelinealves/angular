6a36f53e0b76cd398e44d0748d1b5baded273b21
Jacqueline Alves <jacque.alvesilva@gmail.com>
2020-10-03 18:30:40 -0300

    Add: Atividade da Galeria de Fotos.

---
+++ galeria-fotos/src/app/galeria/galeria.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria',
+  templateUrl: './galeria.component.html',
+  styleUrls: ['./galeria.component.css'],
+})
+export class GaleriaComponent {
+  @Input() titulo: string;
+  @Input() fotos: string[] = [
+    'https://images.unsplash.com/photo-1522100263008-3432d968e398?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
+    'https://images.unsplash.com/photo-1532381959500-9441576dd3b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
+    'https://images.unsplash.com/photo-1514428631868-a400b561ff44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+  ];
+}
