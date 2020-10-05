bcfc5691fcbbeceaefb4ac4aecacd36edbe2b043
Jacqueline Alves <jacque.alvesilva@gmail.com>
2020-10-05 10:33:11 -0300

    Add: Component galeria.component; Create: Strutura do projeto.

--- galeria-fotos/src/app/galeria/galeria.component.ts
+++ galeria-fotos/src/app/galeria/galeria.component.ts
@@ -1,11 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'galeria',
   templateUrl: './galeria.component.html',
   styleUrls: ['./galeria.component.css'],
 })
-export class GaleriaComponent {
-  @Input() titulo: string;
-  @Input() fotos: string[] = [];
+export class GaleriaComponent implements OnInit {
+  @Input() title: string;
+
+  @Input() photos: string[] = [
+    // 'http://localhost:4200/assets/01.jpg',
+    './assets/01.jpg',
+    './assets/02.jpg',
+    './assets/03.jpg',
+    './assets/04.jpg',
+    './assets/05.jpg',
+    './assets/06.jpg',
+    './assets/07.jpg',
+    './assets/08.jpg',
+    './assets/09.jpg',
+    './assets/10.jpg'
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
