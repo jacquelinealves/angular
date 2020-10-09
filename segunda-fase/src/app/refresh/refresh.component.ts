829c9c36c4c7fbe55f26caf32f46444a87583902
Jacqueline Alves <jacque.alvesilva@gmail.com>
2020-10-09 12:12:49 -0300

    Add: Segunda fase dos estudos.

---
+++ segunda-fase/src/app/refresh/refresh.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'refresh',
+  templateUrl: './refresh.component.html',
+  styleUrls: ['./refresh.component.css']
+})
+export class RefreshComponent implements OnInit, OnDestroy {
+
+  @Input() id: number;
+  interval: NodeJS.Timeout;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      console.log(`Component #${this.id} refresh`);
+    }, 2000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+}
