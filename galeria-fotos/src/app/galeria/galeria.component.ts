8027c78daf7f511a8ef37ba8936d4ee944eb518d
Jacqueline Alves <jacque.alvesilva@gmail.com>
2020-10-09 12:13:16 -0300

    Error: Testes não finalizados.

--- galeria-fotos/src/app/galeria/galeria.component.ts
+++ galeria-fotos/src/app/galeria/galeria.component.ts
@@ -1,28 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'galeria',
   templateUrl: './galeria.component.html',
   styleUrls: ['./galeria.component.css'],
 })
-export class GaleriaComponent implements OnInit {
+export class GaleriaComponent {
   @Input() title: string;
+  @Input() photos: string[] = [];
 
-  @Input() photos: string[] = [
-    // 'http://localhost:4200/assets/01.jpg',
-    './assets/01.jpg',
-    './assets/02.jpg',
-    './assets/03.jpg',
-    './assets/04.jpg',
-    './assets/05.jpg',
-    './assets/06.jpg',
-    './assets/07.jpg',
-    './assets/08.jpg',
-    './assets/09.jpg',
-    './assets/10.jpg'
-  ];
+  photoActual: number = 0;
+  totalPhotos: number;
 
-  constructor() {}
-
-  ngOnInit(): void {}
 }
